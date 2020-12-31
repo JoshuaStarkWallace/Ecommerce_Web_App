30f803d1da9cbbd75835adf2487d8c5ef932eb6b
Joshua Wallace <JoshuaStarkWallace@outlook.com>
2020-12-31 16:01:54 -0700

    Added HttpGet AddPage method to the PagesController.cs Created AddPage.cshtml Customized AddPage.cshtml Added HttpPost AddPage method to PagesController.cs #Runs#

--- Ecommerce_Web_App/Areas/Admin/Controllers/PagesController.cs
+++ Ecommerce_Web_App/Areas/Admin/Controllers/PagesController.cs
@@ -25,5 +25,72 @@ namespace Ecommerce_Web_App.Areas.Admin.Controllers
             // Return view with list
             return View(PagesList);
         }
+
+        #region Add Page Methods
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+
+                // Init PageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO Title
+                dto.Title = model.Title;
+
+                // Check for and set Slug
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                // Check that Tile and Slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title of slug already exists.");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            // Set the TempData message
+            TempData["SuccessMessage"] = "You have added a new page!";
+
+            // Redirect
+            return RedirectToAction("AddPage");
+        }
+
+        #endregion
     }
 }
